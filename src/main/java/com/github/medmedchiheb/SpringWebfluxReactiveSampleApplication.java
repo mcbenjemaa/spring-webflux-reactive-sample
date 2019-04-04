bab96026e5fb9221f8f50527bfc9257bff251e42
Mohamed Chiheb Ben Jemaa <benjemaam@webone.local>
2019-04-04 10:37:09 +0100

    intialize project

---
+++ src/main/java/com/github/medmedchiheb/SpringWebfluxReactiveSampleApplication.java
@@ -0,0 +1,13 @@
+package com.github.medmedchiheb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebfluxReactiveSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebfluxReactiveSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/medmedchiheb/SpringWebfluxReactiveSampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.medmedchiheb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringWebfluxReactiveSampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
